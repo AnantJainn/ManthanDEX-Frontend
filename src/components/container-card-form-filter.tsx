91d1c0f869147a57291b2740c4a51eacbdc2d404
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2023-08-08 23:21:23 +0530

    why manthan dex section improved

--- src/components/container-card-form-filter.tsx
+++ src/components/container-card-form-filter.tsx
@@ -8,117 +8,121 @@ import Property1Frame48095586 from "./property1-frame48095586";
 
 const ContainerCardFormFilter: FunctionComponent = memo(() => {
   return (
-    <section className="absolute top-[101.81rem] left-[0rem] w-[90rem] h-[36.25rem] text-center text-[2.5rem] text-darkturquoise font-bai-jamjuree">
-      <div className="absolute top-[-0.06rem] left-[7.5rem] rounded-11xl bg-midnightblue-400 w-[75rem] flex flex-col items-start justify-start">
-        <div className="self-stretch my-0 mx-[!important] absolute top-[3.75rem] left-[0rem] flex flex-col items-center justify-start z-[0]">
-          <div className="self-stretch flex flex-col items-start justify-start">
-            <div className="self-stretch flex flex-col items-start justify-start gap-[5rem]">
-              <div className="self-stretch flex flex-col items-center justify-start gap-[1rem]">
-                <b className="self-stretch relative">Why Manthan DEX</b>
-                <div className="self-stretch relative text-[1rem] tracking-[0.05em] font-light text-gainsboro-300">
-                  Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat
-                  et ornare risus.
-                </div>
-              </div>
-              <div className="self-stretch flex flex-col items-start justify-start gap-[3.75rem]">
-                <div className="self-stretch flex flex-row py-[0rem] px-[4.13rem] items-center justify-between lg:flex-col lg:gap-[5.94rem] lg:items-start lg:justify-start md:flex-col md:gap-[5.94rem] md:items-start md:justify-start sm:flex-col sm:gap-[5.94rem] sm:items-start sm:justify-start">
-                  <Property1Frame480955865
-                    iconSecurity="/-icon-security1.svg"
-                    property1Frame48095586Position="unset"
-                    property1Frame48095586Width="18.25rem"
-                    property1Frame48095586FlexShrink="0"
-                    frameDivAlignSelf="stretch"
-                    frameDivWidth="unset"
-                    frameDivAlignSelf1="stretch"
-                    frameDivAlignSelf2="stretch"
-                    euTellusQuamDisplay="inline-block"
-                    euTellusQuamAlignItems="unset"
-                    euTellusQuamWidth="unset"
-                    euTellusQuamFlexShrink="unset"
-                    euTellusQuamFlex="1"
-                  />
-                  <Property1Frame480955864
-                    iconTrade="/-icon-trade1.svg"
-                    property1Frame48095586Position="unset"
-                    property1Frame48095586Width="18.25rem"
-                    property1Frame48095586FlexShrink="0"
-                    frameDivAlignSelf="stretch"
-                    frameDivWidth="unset"
-                    frameDivAlignSelf1="stretch"
-                    frameDivAlignSelf2="stretch"
-                    euTellusQuamDisplay="inline-block"
-                    euTellusQuamAlignItems="unset"
-                    euTellusQuamWidth="unset"
-                    euTellusQuamFlexShrink="unset"
-                    euTellusQuamFlex="1"
-                  />
-                  <Property1Frame480955863
-                    iconEmptyWallet="/-icon-empty-wallet1.svg"
-                    property1Frame48095586Position="unset"
-                    property1Frame48095586Width="18.25rem"
-                    property1Frame48095586FlexShrink="0"
-                    frameDivAlignSelf="stretch"
-                    frameDivWidth="unset"
-                    frameDivAlignSelf1="stretch"
-                    frameDivAlignSelf2="stretch"
-                    euTellusQuamDisplay="inline-block"
-                    euTellusQuamAlignItems="unset"
-                    euTellusQuamWidth="unset"
-                    euTellusQuamFlexShrink="unset"
-                    euTellusQuamFlex="1"
-                  />
-                </div>
-                <div className="self-stretch flex flex-row py-[0rem] px-[4.13rem] items-center justify-between lg:flex-col lg:gap-[5.94rem] lg:items-start lg:justify-start md:flex-col md:gap-[5.94rem] md:items-start md:justify-start sm:flex-col sm:gap-[5.94rem] sm:items-start sm:justify-start">
-                  <Property1Frame480955862
-                    iconCardCoin="/-icon-card-coin1.svg"
-                    property1Frame48095586Position="unset"
-                    property1Frame48095586Width="18.25rem"
-                    property1Frame48095586FlexShrink="0"
-                    frameDivAlignSelf="stretch"
-                    frameDivWidth="unset"
-                    frameDivAlignSelf1="stretch"
-                    frameDivAlignSelf2="stretch"
-                    euTellusQuamDisplay="inline-block"
-                    euTellusQuamAlignItems="unset"
-                    euTellusQuamWidth="unset"
-                    euTellusQuamFlexShrink="unset"
-                    euTellusQuamFlex="1"
-                  />
-                  <Property1Frame480955861
-                    iconGift="/-icon-gift1.svg"
-                    property1Frame48095586Position="unset"
-                    property1Frame48095586Width="18.25rem"
-                    property1Frame48095586FlexShrink="0"
-                    frameDivAlignSelf="stretch"
-                    frameDivWidth="unset"
-                    frameDivAlignSelf1="stretch"
-                    frameDivAlignSelf2="stretch"
-                    euTellusQuamDisplay="inline-block"
-                    euTellusQuamAlignItems="unset"
-                    euTellusQuamWidth="unset"
-                    euTellusQuamFlexShrink="unset"
-                    euTellusQuamFlex="1"
-                  />
-                  <Property1Frame48095586
-                    iconMenu="/-icon-menu1.svg"
-                    property1Frame48095586Position="unset"
-                    property1Frame48095586Width="18.25rem"
-                    property1Frame48095586FlexShrink="0"
-                    frameDivAlignSelf="stretch"
-                    frameDivWidth="unset"
-                    frameDivAlignSelf1="stretch"
-                    frameDivAlignSelf2="stretch"
-                    euTellusQuamDisplay="inline-block"
-                    euTellusQuamAlignItems="unset"
-                    euTellusQuamWidth="unset"
-                    euTellusQuamFlexShrink="unset"
-                    euTellusQuamFlex="1"
-                  />
+    <section className="relative container-card-form-filter">
+      <div className="container-card-form-filter-content">
+        <section className="absolute top-[101.81rem] left-[0rem] w-[90rem] h-[36.25rem] text-center text-[2.5rem] text-darkturquoise font-bai-jamjuree">
+          <div className="absolute top-[-0.06rem] left-[7.5rem] rounded-11xl bg-midnightblue-400 w-[75rem] flex flex-col items-start justify-start">
+            <div className="self-stretch my-0 mx-[!important] absolute top-[3.75rem] left-[0rem] flex flex-col items-center justify-start z-[0]">
+              <div className="self-stretch flex flex-col items-start justify-start">
+                <div className="self-stretch flex flex-col items-start justify-start gap-[5rem]">
+                  <div className="self-stretch flex flex-col items-center justify-start gap-[1rem]">
+                    <b className="self-stretch relative">Why Manthan DEX</b>
+                    <div className="self-stretch relative text-[1rem] tracking-[0.05em] font-light text-gainsboro-300">
+                      Eu tellus quam id sed ultrices. Integer nunc lectus nisi,
+                      erat et ornare risus.
+                    </div>
+                  </div>
+                  <div className="self-stretch flex flex-col items-start justify-start gap-[3.75rem] ml-20">
+                    <div className="self-stretch flex flex-row py-[0rem] px-[4.13rem] property-frame-row">
+                      <Property1Frame480955865
+                        iconSecurity="/-icon-security1.svg"
+                        property1Frame48095586Position="unset"
+                        property1Frame48095586Width="18.25rem"
+                        property1Frame48095586FlexShrink="0"
+                        frameDivAlignSelf="stretch"
+                        frameDivWidth="unset"
+                        frameDivAlignSelf1="stretch"
+                        frameDivAlignSelf2="stretch"
+                        euTellusQuamDisplay="inline-block"
+                        euTellusQuamAlignItems="unset"
+                        euTellusQuamWidth="unset"
+                        euTellusQuamFlexShrink="unset"
+                        euTellusQuamFlex="1"
+                      />
+                      <Property1Frame480955864
+                        iconTrade="/-icon-trade1.svg"
+                        property1Frame48095586Position="unset"
+                        property1Frame48095586Width="18.25rem"
+                        property1Frame48095586FlexShrink="0"
+                        frameDivAlignSelf="stretch"
+                        frameDivWidth="unset"
+                        frameDivAlignSelf1="stretch"
+                        frameDivAlignSelf2="stretch"
+                        euTellusQuamDisplay="inline-block"
+                        euTellusQuamAlignItems="unset"
+                        euTellusQuamWidth="unset"
+                        euTellusQuamFlexShrink="unset"
+                        euTellusQuamFlex="1"
+                      />
+                      <Property1Frame480955863
+                        iconEmptyWallet="/-icon-empty-wallet1.svg"
+                        property1Frame48095586Position="unset"
+                        property1Frame48095586Width="18.25rem"
+                        property1Frame48095586FlexShrink="0"
+                        frameDivAlignSelf="stretch"
+                        frameDivWidth="unset"
+                        frameDivAlignSelf1="stretch"
+                        frameDivAlignSelf2="stretch"
+                        euTellusQuamDisplay="inline-block"
+                        euTellusQuamAlignItems="unset"
+                        euTellusQuamWidth="unset"
+                        euTellusQuamFlexShrink="unset"
+                        euTellusQuamFlex="1"
+                      />
+                    </div>
+                    <div className="self-stretch flex flex-row py-[0rem] px-[4.13rem] property-frame-row">
+                      <Property1Frame480955862
+                        iconCardCoin="/-icon-card-coin1.svg"
+                        property1Frame48095586Position="unset"
+                        property1Frame48095586Width="18.25rem"
+                        property1Frame48095586FlexShrink="0"
+                        frameDivAlignSelf="stretch"
+                        frameDivWidth="unset"
+                        frameDivAlignSelf1="stretch"
+                        frameDivAlignSelf2="stretch"
+                        euTellusQuamDisplay="inline-block"
+                        euTellusQuamAlignItems="unset"
+                        euTellusQuamWidth="unset"
+                        euTellusQuamFlexShrink="unset"
+                        euTellusQuamFlex="1"
+                      />
+                      <Property1Frame480955861
+                        iconGift="/-icon-gift1.svg"
+                        property1Frame48095586Position="unset"
+                        property1Frame48095586Width="18.25rem"
+                        property1Frame48095586FlexShrink="0"
+                        frameDivAlignSelf="stretch"
+                        frameDivWidth="unset"
+                        frameDivAlignSelf1="stretch"
+                        frameDivAlignSelf2="stretch"
+                        euTellusQuamDisplay="inline-block"
+                        euTellusQuamAlignItems="unset"
+                        euTellusQuamWidth="unset"
+                        euTellusQuamFlexShrink="unset"
+                        euTellusQuamFlex="1"
+                      />
+                      <Property1Frame48095586
+                        iconMenu="/-icon-menu1.svg"
+                        property1Frame48095586Position="unset"
+                        property1Frame48095586Width="18.25rem"
+                        property1Frame48095586FlexShrink="0"
+                        frameDivAlignSelf="stretch"
+                        frameDivWidth="unset"
+                        frameDivAlignSelf1="stretch"
+                        frameDivAlignSelf2="stretch"
+                        euTellusQuamDisplay="inline-block"
+                        euTellusQuamAlignItems="unset"
+                        euTellusQuamWidth="unset"
+                        euTellusQuamFlexShrink="unset"
+                        euTellusQuamFlex="1"
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
     </section>
   );
